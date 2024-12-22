9d520db5b029b60209fc9a2d9ee3e09ce7c41e32
lsbrel <lucas.81.gabriel@gmail.com>
2024-12-22 20:41:42 -0300

    docker-compose adicionado e configurado para rodar sistema

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-console.log("entry_point");
+import { FastifyInstance } from "fastify";
+import { Router } from "./src/presentation/router/router";
+require("dotenv").config();
+
+const fastify: FastifyInstance = require("fastify")({ logger: true });
+
+const router: Router = new Router(fastify);
+router.start();
+
+fastify.listen({
+  port: process.env.PORT as number | undefined,
+});
