b040357cde8690fa1cdc579188294c485fed943d
lsbrel <lucas.81.gabriel@gmail.com>
2025-01-14 22:53:50 -0300

    Atualizações em Login

--- src/application/controllers/LoginController.ts
+++ src/application/controllers/LoginController.ts
@@ -3,52 +3,51 @@ import LoginServices from "../../application/services/LoginServices";
 import Jwt from "../../application/Facades/Jwt";
 
 export default class LoginController {
+  private loginServices: LoginServices;
 
-    private loginServices: LoginServices;
+  constructor() {
+    this.loginServices = new LoginServices();
+  }
 
-    constructor() {
-        this.loginServices = new LoginServices();
+  public login = async (request: FastifyRequest, reply: FastifyReply) => {
+    const data = request.body as any;
+
+    const authUser = await this.loginServices.auth(data.email, data.password);
+
+    if (!authUser)
+      return reply.code(401).send({
+        status: 401,
+        message: "User not Authorized",
+        content: "",
+      });
+
+    await this.loginServices.saveOnHistory();
+
+    return reply.code(200).send({
+      status: 200,
+      message: "Login confirmed",
+      content: {
+        token: Jwt.make(authUser),
+      },
+    });
+  };
+
+  public getUserLoginHistory = async (
+    request: FastifyRequest,
+    reply: FastifyReply
+  ) => {
+    try {
+      const { id } = request.params as any;
+
+      const userLoginHistory = await this.loginServices.getUserLoginHistory(id);
+
+      reply.code(200).send({
+        status: 200,
+        message: "Login history has been retrieved",
+        content: userLoginHistory,
+      });
+    } catch (error) {
+      reply.send(error);
     }
-
-
-    public login = async (request: FastifyRequest, reply: FastifyReply) => {
-
-        const data = request.body as any
-
-        const authUser = await this.loginServices.auth(data.email, data.password)
-
-        if (!authUser)
-            reply.code(401).send({
-                "status": 401,
-                "message": "User not Authorized"
-            })
-
-        await this.loginServices.saveOnHistory();
-
-        return reply.code(200).send({
-            "status": 200,
-            "message": "Login confirmed",
-            "content": {
-                "token": Jwt.make(authUser)
-            }
-        })
-    }
-
-
-    public getUserLoginHistory = async (request: FastifyRequest, reply: FastifyReply) => {
-        try {
-            const { id } = request.params as any
-
-            const userLoginHistory = await this.loginServices.getUserLoginHistory(id)
-
-            reply.code(200).send({
-                "status": 200,
-                "message": "Login history has been retrieved",
-                "content": userLoginHistory
-            })
-        } catch (error) {
-
-            reply.send(error)
-        }
-    }
+  };
 }
--- src/application/services/LoginServices.ts
+++ src/application/services/LoginServices.ts
@@ -1,50 +1,48 @@
 import LoginHistoryDTO from "../../infrastructure/dto/LoginHistoryDTO";
-import UserDTO from "../../infrastructure/dto/UserDTO"
-import UserRepositoryImplementation from "../../infrastructure/repository/UserRepositoryImplementation"
-import Hash from "../Facades/Hash"
+import UserDTO from "../../infrastructure/dto/UserDTO";
+import UserRepositoryImplementation from "../../infrastructure/repository/UserRepositoryImplementation";
+import Hash from "../Facades/Hash";
 
 export default class LoginServices {
+  private loginHistoryUSerId: number = 0;
 
-    private loginHistoryUSerId: number = 0;
+  public auth = async (email: string, password: string) => {
+    const userRepository: UserRepositoryImplementation =
+      new UserRepositoryImplementation();
+    const user: any = await userRepository.findByEmail(email);
 
-    public auth = async (email: string, password: string) => {
-        const userRepository: UserRepositoryImplementation = new UserRepositoryImplementation()
-        const user: any = await userRepository.findByEmail(email)
+    if (!user) return false;
 
-        this.setLoginHistoryUserId(user.id)
+    this.setLoginHistoryUserId(user.id);
 
-        if (user.password === Hash.make(password))
-            return {
-                id: user.id,
-                email: user.email,
-                name: user.name
-            };
+    if (user.password === Hash.make(password))
+      return {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      };
+    else return false;
+  };
 
-        else
-            return false
+  public saveOnHistory = async () => {
+    try {
+      const loginHistory = new LoginHistoryDTO({
+        user_id: this.loginHistoryUSerId,
+      });
+      await loginHistory.save();
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-    public saveOnHistory = async () => {
-        try {
-            const loginHistory = new LoginHistoryDTO({
-                user_id: this.loginHistoryUSerId
-            })
-            await loginHistory.save()
+  public getUserLoginHistory = async (userId: number) => {
+    const userRepository: UserRepositoryImplementation =
+      new UserRepositoryImplementation();
 
-        } catch (e) {
-            console.log(e)
-        }
-    }
+    return userRepository.getLoginHistory(userId);
+  };
 
-    public getUserLoginHistory = async (userId: number) => {
-        const userRepository: UserRepositoryImplementation = new UserRepositoryImplementation();
-
-        return userRepository.getLoginHistory(userId)
-    }
-
-    private setLoginHistoryUserId = (id: number) => {
-        this.loginHistoryUSerId = id
-    }
-
-
+  private setLoginHistoryUserId = (id: number) => {
+    this.loginHistoryUSerId = id;
+  };
 }
